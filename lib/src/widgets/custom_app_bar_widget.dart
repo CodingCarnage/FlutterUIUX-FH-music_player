a00fb0863a07adb01e1038e3a2e516d73ab5ae6e
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-28 15:35:44 -0600

    Se creo un AppBar personalizado.

---
+++ lib/src/widgets/custom_app_bar_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.only(top: 20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          children: <Widget>[
+            Icon(FontAwesomeIcons.chevronLeft),
+            const Spacer(),
+            Icon(FontAwesomeIcons.commentAlt),
+            const SizedBox(width: 20.0),
+            Icon(FontAwesomeIcons.headphonesAlt),
+            const SizedBox(width: 20.0),
+            Icon(FontAwesomeIcons.externalLinkAlt),
+          ],
+        ),
+      ),
+    );
+  }
+}
