f2b4f986e08c18af3e777350b6a40a8acb86cf6b
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-28 15:36:09 -0600

    Se agrego el CustomAppBar.

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'package:music_player/src/widgets/custom_app_bar_widget.dart';
+
 class MusicPlayerPage extends StatelessWidget {
   const MusicPlayerPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola mundo'),
+      body: Column(
+        children: <Widget>[
+          CustomAppBar(),
+        ],
       ),
     );
   }
