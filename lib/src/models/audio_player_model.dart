5b671ca121425a3d25f120a5f1003631def05bcd
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-04-02 20:19:25 -0600

    Se agregaron varias caracteristicas al model para determinar la duración total y actual de la canción.

--- lib/src/models/audio_player_model.dart
+++ lib/src/models/audio_player_model.dart
@@ -10,6 +10,43 @@ class AudioPlayerModel with ChangeNotifier {
     notifyListeners();
   }
 
+  Duration _songDuration = new Duration(milliseconds: 0);
+
+  Duration get songDuration => this._songDuration;
+
+  set songDuration(Duration songDuration) {
+    this._songDuration = songDuration;
+    notifyListeners();
+  }
+
+  Duration _songCurrentTime = new Duration(milliseconds: 0);
+
+  Duration get songCurrentTime => this._songCurrentTime;
+
+  set songCurrentTime(Duration songCurrentTime) {
+    this._songCurrentTime = songCurrentTime;
+    notifyListeners();
+  }
+
+  double get songPercentage => (this._songDuration.inSeconds > 0) 
+                               ? this._songCurrentTime.inSeconds / this._songDuration.inSeconds 
+                               : 0;
+
+  String printDuration(Duration duration) {
+    String twoDigits(int number) {
+      if (number >= 10)
+        return '$number';
+      return '0$number';
+    }
+
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return '$twoDigitMinutes:$twoDigitSeconds';
+  }
+
+  String get songTotalDuration => this.printDuration(this._songDuration);
+  String get songCurrentDuration => this.printDuration(this._songCurrentTime);
+
   AnimationController _animationController;
 
   AnimationController get animationController => this._animationController;
