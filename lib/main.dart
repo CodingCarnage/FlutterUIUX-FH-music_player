821534459514df6407fd875af1ddf88454785dda
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-04-02 19:57:12 -0600

    Se agrego un nuevo provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import 'package:music_player/src/models/audio_player_model.dart';
+
 import 'package:music_player/src/pages/music_player_page.dart';
 
 import 'package:music_player/src/theme/theme.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Music Player',
-      theme: myTheme,
-      home: MusicPlayerPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => new AudioPlayerModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Music Player',
+        theme: myTheme,
+        home: MusicPlayerPage(),
+      ),
     );
   }
 }
